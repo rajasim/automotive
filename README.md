242efb6f9ed7bdd7b99c4946aaf5b8e5da62d75f
miaoda <miaoda@baidu.com>
2025-11-27 13:15:10 +0530

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Welcome to Your Miaoda Project
+Miaoda Application Link URL
+    URL:https://medo.dev/projects/app-7qzbw8tr7aip
+
+# Welcome to Your Miaoda Project
 
 ## Project Info
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-import { miaodaDevPlugin } from "miaoda-sc-plugin";
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({
-      svgrOptions: {
-        icon: true, exportType: 'named', namedExport: 'ReactComponent', }, }), miaodaDevPlugin()],
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
 });
