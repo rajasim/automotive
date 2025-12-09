98023aca570c677646cf1a413b38b8b677271a67
miaoda <miaoda@baidu.com>
2025-12-09 15:09:44 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,24 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Header from '@/components/common/Header';
 import Footer from '@/components/common/Footer';
 import { Toaster } from '@/components/ui/toaster';
-import Scrolltotop from './components/Scrolltotop';
 import routes from './routes';
+import Scrolltotop from './components/Scrolltotop'; 
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Scrolltotop />
+      <Scrolltotop /> 
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">
           <Routes>
-            {routes
-              .filter(route => route.visible !== false) // Only render visible routes
-              .map((route, index) => (
-                <Route key={index} path={route.path} element={route.element} />
-              ))}
+            {routes.map((route, index) => (
+              <Route
+                key={index}
+                path={route.path}
+                element={route.element}
+              />
+            ))}
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </main>
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Home from './pages/Home';
 import Courses from './pages/Courses';
 import CourseDetail from './pages/CourseDetail';
@@ -6,6 +5,7 @@ import Blog from './pages/Blog';
 import BlogDetail from './pages/BlogDetail';
 import Contact from './pages/Contact';
 import PaymentSuccess from './pages/PaymentSuccess';
+import Article from './pages/Article'; // New page for article list
 
 import type { ReactNode } from 'react';
 
@@ -47,6 +47,8 @@ const routes: RouteConfig[] = [
     element: <BlogDetail />,
     visible: false
   },
+  
+  
   {
     name: 'Contact',
     path: '/contact',
